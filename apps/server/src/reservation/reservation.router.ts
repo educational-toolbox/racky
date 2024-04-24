e1fbfa13b302f4a6f1808e5b28b2cecacd080b64
clmtl <bk201li@protonmail.com>
2024-04-24 13:48:06 +0200

    Add rest of crud

--- apps/server/src/reservation/reservation.router.ts
+++ apps/server/src/reservation/reservation.router.ts
@@ -5,6 +5,7 @@ import {
   reservationSchemaRead,
   reservationSchemaWrite,
 } from './reservation.schema';
+import { z } from 'zod';
 
 @Injectable()
 export class ReservationRouter {
@@ -14,10 +15,33 @@ export class ReservationRouter {
   ) {}
 
   reservationRouter = this.trpc.router({
+    findAll: this.trpc.procedure
+      .meta({ openapi: { method: 'GET', path: '/reservation' } })
+      .output(reservationSchemaRead.array())
+      .query(() => this.reservationService.findAll()),
+
+    findOne: this.trpc.procedure
+      .meta({ openapi: { method: 'GET', path: '/reservation/:id' } })
+      .input(z.object({ id: z.string() }))
+      .output(reservationSchemaRead.nullable())
+      .query(({ input }) => this.reservationService.findOne(input.id)),
+
     create: this.trpc.protectedProcedure
       .meta({ openapi: { method: 'POST', path: '/reservation' } })
       .input(reservationSchemaWrite)
       .output(reservationSchemaRead)
       .mutation(({ input }) => this.reservationService.create(input, '911')),
+
+    update: this.trpc.protectedProcedure
+      .meta({ openapi: { method: 'PUT', path: '/reservation' } })
+      .input(reservationSchemaRead)
+      .output(reservationSchemaRead)
+      .mutation(({ input }) => this.reservationService.update(input, '911')),
+
+    delete: this.trpc.protectedProcedure
+      .meta({ openapi: { method: 'DELETE', path: '/reservation/:id' } })
+      .input(z.object({ id: z.string() }))
+      .output(reservationSchemaRead)
+      .mutation(({ input }) => this.reservationService.delete(input.id)),
   });
 }
--- apps/server/src/reservation/reservation.service.ts
+++ apps/server/src/reservation/reservation.service.ts
@@ -1,17 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from '@educational-toolbox/racky-api/database/database.service';
-import { ReservationWrite } from './reservation.schema';
+import { ReservationRead, ReservationWrite } from './reservation.schema';
 
 @Injectable()
 export class ReservationService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  async create(reservation: ReservationWrite, userId: string) {
+  findAll() {
+    return this.databaseService.reservation.findMany();
+  }
+
+  findOne(reservationId: string) {
+    return this.databaseService.reservation.findUnique({
+      where: { id: reservationId },
+    });
+  }
+
+  create(reservation: ReservationWrite, userId: string) {
     return this.databaseService.reservation.create({
       data: {
         ...reservation,
         user: { connect: { id: userId } },
       },
+    });
+  }
+
+  update(reservation: ReservationRead, userId: string) {
+    return this.databaseService.reservation.update({
+      where: { id: reservation.id, userId },
+      data: reservation,
+    });
+  }
+
+  delete(reservationId: string) {
+    return this.databaseService.reservation.delete({
+      where: { id: reservationId },
     });
   }
 }
