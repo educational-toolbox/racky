a525b7a5ef22b4cc9131e42ce36c9e10f6fcac01
Dmitrii Kopenkin (s-dkopenkin@digikare.com) <s-dkopenkin@digikare.com>
2024-06-05 21:42:15 +0200

    fix: cls + separate into mini-components

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -38,7 +38,7 @@ export default function DashboardLayout({ children }: PropsWithChildren) {
   const crumbs = path.split("/").filter(Boolean);
   const { items } = useMenuItems();
 
-  const fixedPathname = path.endsWith("/") ? path : path + "/";
+  const cleanPathname = path.endsWith("/") ? path : path + "/";
 
   return (
     <>
@@ -47,92 +47,15 @@ export default function DashboardLayout({ children }: PropsWithChildren) {
         <div className="flex min-h-screen w-full flex-col bg-muted/40">
           {/* DESKTOP MENU */}
           <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-            <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-              <Link
-                href="#"
-                className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
-              >
-                <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
-                <span className="sr-only">Acme Inc</span>
-              </Link>
-              <DesktopMenu items={items} pathname={fixedPathname} />
-            </nav>
-            <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <ThemeSwitcher />
-                </TooltipTrigger>
-                <TooltipContent side="right">Change theme</TooltipContent>
-              </Tooltip>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Link
-                    href="#"
-                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                  >
-                    <Settings className="h-5 w-5" />
-                    <span className="sr-only">Settings</span>
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent side="right">Settings</TooltipContent>
-              </Tooltip>
-            </nav>
+            <DesktopMenuWrapper items={items} pathname={cleanPathname} />
           </aside>
 
           <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+            {/* MOBILE MENU */}
             <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-              {/* MOBILE MENU */}
-              <Sheet>
-                <SheetTrigger asChild>
-                  <Button size="icon" variant="outline" className="sm:hidden">
-                    <PanelLeft className="h-5 w-5" />
-                    <span className="sr-only">Toggle Menu</span>
-                  </Button>
-                </SheetTrigger>
-                <SheetContent side="left" className="sm:max-w-xs">
-                  <nav className="grid gap-6 text-lg font-medium">
-                    <MobileMenu items={items} pathname={fixedPathname} />
-                  </nav>
-                </SheetContent>
-              </Sheet>
-
-              {/* BREADCRUMBS */}
-              <Breadcrumb className="hidden md:flex">
-                <BreadcrumbList>
-                  {crumbs.length === 0 ? (
-                    <BreadcrumbItem>
-                      <BreadcrumbPage>Dashboard</BreadcrumbPage>
-                    </BreadcrumbItem>
-                  ) : (
-                    <BreadcrumbItem>
-                      <BreadcrumbLink asChild>
-                        <Link href="/">Dashboard</Link>
-                      </BreadcrumbLink>
-                    </BreadcrumbItem>
-                  )}
-                  {crumbs.length > 0 && <BreadcrumbSeparator />}
-                  {crumbs.map((crumb, index) => {
-                    if (index === crumbs.length - 1) {
-                      return (
-                        <BreadcrumbItem key={index}>
-                          <BreadcrumbPage>{slugToTitle(crumb)}</BreadcrumbPage>
-                        </BreadcrumbItem>
-                      );
-                    }
-                    return (
-                      <Fragment key={index}>
-                        <BreadcrumbItem>
-                          <BreadcrumbLink asChild>
-                            <Link href="#">{crumb}</Link>
-                          </BreadcrumbLink>
-                        </BreadcrumbItem>
-                        <BreadcrumbSeparator />
-                      </Fragment>
-                    );
-                  })}
-                </BreadcrumbList>
-              </Breadcrumb>
-              <div className="ml-auto">
+              <MobileMenuWrapper items={items} pathname={cleanPathname} />
+              <DashboardBreadcrumbs crumbs={crumbs} />
+              <div className="ml-auto w-7 h-7">
                 <UserButton />
               </div>
             </header>
@@ -149,6 +72,107 @@ export default function DashboardLayout({ children }: PropsWithChildren) {
         <RedirectToSignIn />
       </SignedOut>
     </>
+  );
+}
+
+function MobileMenuWrapper(props: {
+  items: readonly MenuItem[];
+  pathname: string;
+}) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size="icon" variant="outline" className="sm:hidden">
+          <PanelLeft className="h-5 w-5" />
+          <span className="sr-only">Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="sm:max-w-xs">
+        <nav className="grid gap-6 text-lg font-medium">
+          <MobileMenu items={props.items} pathname={props.pathname} />
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+function DesktopMenuWrapper(props: {
+  items: readonly MenuItem[];
+  pathname: string;
+}) {
+  return (
+    <>
+      <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+        <Link
+          href="#"
+          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+        >
+          <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+          <span className="sr-only">Acme Inc</span>
+        </Link>
+        <DesktopMenu items={props.items} pathname={props.pathname} />
+      </nav>
+      <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <ThemeSwitcher />
+          </TooltipTrigger>
+          <TooltipContent side="right">Change theme</TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href="#"
+              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+            >
+              <Settings className="h-5 w-5" />
+              <span className="sr-only">Settings</span>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="right">Settings</TooltipContent>
+        </Tooltip>
+      </nav>
+    </>
+  );
+}
+
+function DashboardBreadcrumbs({ crumbs }: { crumbs: string[] }) {
+  return (
+    <Breadcrumb className="hidden md:flex">
+      <BreadcrumbList>
+        {crumbs.length === 0 ? (
+          <BreadcrumbItem>
+            <BreadcrumbPage>Dashboard</BreadcrumbPage>
+          </BreadcrumbItem>
+        ) : (
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Dashboard</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        )}
+        {crumbs.length > 0 && <BreadcrumbSeparator />}
+        {crumbs.map((crumb, index) => {
+          if (index === crumbs.length - 1) {
+            return (
+              <BreadcrumbItem key={index}>
+                <BreadcrumbPage>{slugToTitle(crumb)}</BreadcrumbPage>
+              </BreadcrumbItem>
+            );
+          }
+          return (
+            <Fragment key={index}>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href="#">{crumb}</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+            </Fragment>
+          );
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
   );
 }
 
