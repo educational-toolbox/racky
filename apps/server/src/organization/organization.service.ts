018a98fc23bb4798d5772c03af5b9600943970ae
Dmitrii Kopenkin (s-dkopenkin@digikare.com) <s-dkopenkin@digikare.com>
2024-06-19 21:16:42 +0200

    feat: bootstrap main org functionality

--- apps/server/src/organization/organization.service.ts
+++ apps/server/src/organization/organization.service.ts
@@ -5,12 +5,50 @@ import { DatabaseService } from '../database/database.service';
 export class OrganizationService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  async create() {
+  async create(name: string, zone: string) {
     return this.databaseService.organization.create({
-      data: {
-        name: '',
-        zone: '',
-      },
+      data: { name, zone },
+    });
+  }
+
+  async edit(id: string, name: string, zone: string) {
+    return this.databaseService.organization.update({
+      where: { id },
+      data: { name, zone },
+    });
+  }
+
+  async addUser(organizationId: string, userId: string) {
+    return this.databaseService.organization.update({
+      where: { id: organizationId },
+      data: { users: { connect: { id: userId } } },
+    });
+  }
+
+  async removeUser(organizationId: string, userId: string) {
+    return this.databaseService.organization.update({
+      where: { id: organizationId },
+      data: { users: { disconnect: { id: userId } } },
+    });
+  }
+
+  async createInvite(email: string, organizationId: string) {
+    return this.databaseService.organizationInvite.create({
+      data: { email, organizationId },
+    });
+  }
+
+  async checkInviteExistence(inviteId: string) {
+    return this.databaseService.organizationInvite.findUnique({
+      where: { id: inviteId, valid: true },
+      select: { id: true },
+    });
+  }
+
+  async validateInvite(inviteId: string) {
+    return this.databaseService.organizationInvite.update({
+      where: { id: inviteId, valid: true },
+      data: { valid: false },
     });
   }
 }
